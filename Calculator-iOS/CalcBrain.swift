4089c72ff69a4bdc4d657f84316480ddca574331
Jonathan Yaniv <johnitravolta@gmail.com>
2015-07-02 02:53:52 +0300

    First version of Calculator. Written in MVC.

---
+++ Calculator-iOS/CalcBrain.swift
@@ -0,0 +1,112 @@
+//
+//  CalcBrain.swift
+//  Calculator
+//
+//  Created by Jonathan Yaniv on 7/1/15.
+//  Copyright (c) 2015 Jonathan Yaniv. All rights reserved.
+//
+
+import Foundation
+
+class CalcBrain {
+    
+    private enum Op: Printable {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    //  The stack that keeps all operands and operators entered by the user.
+    private var opStack = [Op]()
+    
+    //  The dictionary that keeps all known operators.
+    private var knownOps = [String:Op]()
+    
+    init() {
+        func learnOp(op: Op) {
+            knownOps[op.description] = op
+        }
+        
+        learnOp(Op.BinaryOperation("×", *))
+        learnOp(Op.BinaryOperation("÷") { $1 / $0 })
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.BinaryOperation("−") { $1 - $0 })
+        learnOp(Op.UnaryOperation("√", sqrt))
+    }
+    
+    private func evaluate(var stack: Array<Op>) -> (result: Double?, remaining: [Op]) {
+        if !stack.isEmpty {
+            let op = stack.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, stack)
+                
+            case .UnaryOperation(_, let operation):
+                let evalResult = evaluate(stack)
+                if let operand = evalResult.result {
+                    return (operation(operand), evalResult.remaining)
+                }
+                
+            case .BinaryOperation(_, let operation):
+                let evalResult1 = evaluate(stack)
+                if let op1 = evalResult1.result {
+                    let evalResult2 = evaluate(evalResult1.remaining)
+                    if let op2 = evalResult2.result {
+                        return (operation(op1, op2), evalResult2.remaining)
+                    }
+                }
+            }
+        }
+        
+        return (nil, stack)
+    }
+    
+    //
+    //  Evaluates the opStack
+    private func evaluate() -> Double? {
+        let (result, remaining) = evaluate(opStack)
+        println("\(opStack) = \(result) with remaining \(remaining)")
+        
+        return result
+    }
+
+    //
+    //  Adds the specified operand to the opStack.
+    //
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        let result = evaluate()
+        
+        return result
+    }
+    
+    //
+    //  Adds the specified operation to the opStack to be evaluated.
+    //
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        
+        let result = evaluate()
+        
+        return result
+    }
+    
+    func reset() {
+        opStack = [Op]()
+    }
+}
--- Calculator-iOS/ViewController.swift
+++ Calculator-iOS/ViewController.swift
@@ -1,14 +1,79 @@
 //
 //  ViewController.swift
-//  Calculator-iOS
+//  Calculator
 //
-//  Created by Jonathan Yaniv on 7/2/15.
+//  Created by Jonathan Yaniv on 7/1/15.
 //  Copyright (c) 2015 Jonathan Yaniv. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+
+    //  Result display label
+    @IBOutlet weak var display: UILabel!
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set {
+            userStartedTyping = false
+            
+            if newValue == 0 {
+                display.text = "0"
+            } else {
+                display.text = "\(newValue)"
+            }
+        }
+    }
+    
+    var userStartedTyping = false
+    
+    var brain = CalcBrain()
+    
+    //  
+    //  This method is called when pressing on a button.
+    //
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userStartedTyping {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userStartedTyping = true
+        }
+    }
+    
+    @IBAction func enter() {
+        userStartedTyping = false
+        
+        if let result = self.brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    @IBAction func performOperation(sender: UIButton) {
+        if userStartedTyping {
+            enter()
+        }
+        
+        if let operation = sender.currentTitle {
+            if let result = self.brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func reset() {
+        self.brain.reset()
+        displayValue = 0
+    }
     
 }
-
